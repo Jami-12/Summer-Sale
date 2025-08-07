0e497eff522bbba0040819c717741984e9900c79
mujjaddid-34 <mujaddidahmed883@gmail.com>
2025-08-08 01:23:36 +0600

    Start the journey

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const nameList = document.getElementById("nameList");
 const total = document.getElementById("total");
-const discountTotal = document.getElementById("discountTotal")
+const discountTotal = document.getElementById("discountTotal");
+const purchaseBtn = document.getElementById("purchaseBtn");
+const applyBtn = document.getElementById("applyBtn");
+const inputCoupon = document.getElementById("inputCoupon");
+const discountContainer = document.getElementById("discount");
+applyBtn.disabled = true;
+purchaseBtn.disabled = true;
 let totalPrice = 0;
+
+if (totalPrice > 0) {
+  applyBtn.disabled = false;
+ applyBtn.classList.add("bg-gray-400");
+  applyBtn.classList.remove("bg-pink-500");
+}
 const handClick = (data) => {
-    const name = data.childNodes[1].childNodes[5].innerText
+ 
+    const name = data.childNodes[1].childNodes[5].innerText;
     const PriceWithTk = data.childNodes[1].childNodes[7].innerText;
     const price = PriceWithTk.split(" ")[0];
     const priceNumber = parseInt (price);
@@ -14,5 +27,40 @@ const handClick = (data) => {
     const li = document.createElement("li");
     li.innerHTML = name;
     nameList.appendChild(li);
+    if (totalPrice > 0){
+    purchaseBtn.disabled = false;
 }
 
+    if(totalPrice >= 200){
+        applyBtn.disabled = false;
+    }
+};
+
+applyBtn.addEventListener("click",() => {
+    if(inputCoupon.value == ""){
+        alert("Input Kano Kunta Leikka Ao Ba 😕")
+        return;
+    }
+
+    if(inputCoupon.value == "SELL200"){
+    const discountRate = 0.2;
+    const discount = totalPrice * discountRate;
+    const totalPriceWithDiscount = totalPrice - discount;
+    discountContainer.innerText = discount.toFixed(2);
+    discountTotal.innerText = totalPriceWithDiscount;
+    }else{
+        alert("Cupone Milse Nah Rebo 🤥")
+    }
+   
+})
+
+
+
+purchaseBtn.addEventListener("click" , () => {
+   totalPrice = 0;
+   total.innerText = "00";
+   discountTotal.innerText = "00";
+   discountContainer.innerText = "00";
+   nameList.innerHTML = "";
+   inputCoupon.innerText = "";
+});
